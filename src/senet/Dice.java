1ce9d63e490445f20148b115a6163f0e801faa44
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-01-08 19:18:22 +0100

    Add functionality to Dice.throwDice()
    This dice throw can only ever return a certain set of integers. Only 1, 2, 3, 4 & 6.

--- src/senet/Dice.java
+++ src/senet/Dice.java
@@ -1,12 +1,26 @@
 package senet;
 
+import java.util.Random;
+
 public class Dice {
 
 	public Dice() {
 		
 	}
 	
+	/**
+	 * A dice throw that can only return a certain set of integers
+	 * @return either 1, 2, 3, 4 or 6
+	 */
 	public int throwSticks() {
-		return 1;
+		int n;
+		while (true) {
+			Random random = new Random();
+			n = random.nextInt(6) + 1;
+			if (n != 5) {
+				break;
+			}
+		}
+		return n;
 	}
 }
