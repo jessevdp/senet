74a9670be5b6ffb1cfd65677927e76a0dd7d5125
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-01-08 19:56:12 +0100

    Update Board printing characters
    The only point of this commit is to better match the screen shown in the assignment.
    
    This update is purely visual.

--- src/senet/Board.java
+++ src/senet/Board.java
@@ -2,28 +2,42 @@ package senet;
 
 public class Board {
 	private Player[] squares;
-
+	
+	public Board(Player[] squares) {
+		this.squares = squares;
+	}
+	
 	public Board() {
 		this.squares = new Player[30];
 	}
 	
 	public void print() {
+		System.out.println("+----------+");
+		
+		System.out.print('|');
 		for (int i = 0; i < 10; i++) {
 			printSquare(squares[i]);
 		}
-		System.out.print('\n');
+		System.out.print("|\n");
+		
+		System.out.print('|');
 		for (int i = 19; i > 9; i--) {
 			printSquare(squares[i]);
 		}
-		System.out.print('\n');
+		System.out.print("|\n");
+		
+		System.out.print('|');
 		for (int i = 20; i < 30; i++) {
 			printSquare(squares[i]);
 		}
+		System.out.print("|\n");
+		
+		System.out.println("+----------+");
 	}
 	
 	private void printSquare(Player square) {
 		if (square == null) {
-			System.out.print('.');
+			System.out.print('\u00b7');
 			return;
 		}
 		char character = square.getColorsign();
