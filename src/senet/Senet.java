849e94bb2fd2b54e93ae10f2725946bc273e02a4
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-01-09 12:09:43 +0100

    Remove old tests from Senet.play

--- src/senet/Senet.java
+++ src/senet/Senet.java
@@ -18,32 +18,13 @@ public class Senet {
 	public void play() {
 		System.out.println("Let's play Senet!");
 		
-		// TEST: Dice
-		int points = dice.throwSticks();
-		System.out.println("Dice throw: " + points);
-		
-		// TEST: Player
-		players[0] = new Player("John", 'x');
-		players[1] = new Player("Jane", 'o');
-		System.out.println("Player print: " + players[0].getPrint());
-		
-		// TEST: Board
-		this.board = new Board(0, players[0], players[1]);
-		System.out.println("Board print:");
-		board.print();
-		
-		// TEST Board.countPawns
-		int count = board.countPawns(players[0]);
-		System.out.println("Amount of pawns for player 1: " + count);
-		
-		// TEST: Board.getPawnLocations
-		ArrayList<Integer> locations = board.getPawnLocations(players[0]);
-		System.out.println("Locations of pawns for player 1: " + locations.toString() + " (this is 0 based)");
-		
-		// TEST: this.getPlayers
+		// Get our players
 		players = getPlayers();
 		
-		// TEST: this.playTurn()
+		// Prepare our board
+		this.board = new Board(0, players[0], players[1]);
+		
+		// Play a turn
 		boolean success = playTurn(players[0]);
 	}
 	
