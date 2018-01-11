0ebc4370ed76dd83640489cfa62ea1ca6d87eb1d
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-01-11 09:35:22 +0100

    Introduce Output.java
    This class is meant to help organise and standarderize most of the output messages to the players.

--- src/senet/Senet.java
+++ src/senet/Senet.java
@@ -4,10 +4,13 @@ public class Senet {
 	Dice dice;
 	
 	Input input;
+	Output output;
 
 	public Senet () {
 		this.dice = new Dice();
+		
 		this.input = new Input();
+		this.output = new Output();
 	}
 	
 	public void play () {
