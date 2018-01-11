581b122a98fe6bbd5c0c8b689c70b9064866f72e
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-01-11 11:16:36 +0100

    Add Board.getPawnPositions method
    This method gets the locations for all the pawns of a player.

--- src/senet/Board.java
+++ src/senet/Board.java
@@ -1,5 +1,7 @@
 package senet;
 
+import java.util.ArrayList;
+
 public class Board {
 	private Player[] squares;
 	
@@ -51,6 +53,30 @@ public class Board {
 		}
 		char character = square.getColorsign();
 		System.out.print(character);
+	}
+	
+	/**
+	 * Get the locations for all the pawns of a player
+	 * @param player
+	 * @return positions
+	 */
+	public int[] getPawnPositions (Player player) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int[] positions;
+		
+		for (int i = 0; i < squares.length; i++) {
+			if (squares[i] == player) {
+				list.add(i + 1); // +1 to make up for the fact that the array is 0 based
+			}
+		}
+		
+		// ArrayList<Integer> -> int[]
+		positions = new int[list.size()];
+		for (int i = 0; i < positions.length; i++) {
+			positions[i] = list.get(i).intValue();
+		}
+		
+		return positions;
 	}
 	
 }
