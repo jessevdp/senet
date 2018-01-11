7f4bdcffb42d072dc2dd0365d5248d7d5115e893
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-01-11 09:49:38 +0100

    Add game-mode selection to Senet.play

--- src/senet/Senet.java
+++ src/senet/Senet.java
@@ -2,6 +2,7 @@ package senet;
 
 public class Senet {
 	Dice dice;
+	Board board;
 	
 	Input input;
 
@@ -11,7 +12,16 @@ public class Senet {
 	}
 	
 	public void play () {
+		System.out.println("Let's play Senet!");
 		
+		int mode = input.selectInt(new int[] {0, 1, 2, 3}, "Before we begin: Would you like to start a normal game (0) or a test position?");
+		
+		if (mode == 0) {
+			String[] names = input.getNames(2);
+			// TODO: determine starter to get players
+		} else {
+			// TODO: add test players
+		}
 	}
 	
 }
