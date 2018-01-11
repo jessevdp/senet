d9fa7483dd973efe16f904a0cd614759f41a20bf
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2018-01-11 09:39:14 +0100

    Add Output.turn method
    Displays a turn notice for a player

--- src/senet/Output.java
+++ src/senet/Output.java
@@ -1,10 +1,22 @@
 package senet;
 
 public class Output {
-
+	/**
+	 * Display a welcome message
+	 */
 	public void intro () {
 		System.out.println("Welcome, let's play some Senet!");
 		System.out.println("-------------------------------");
 	}
-
+	
+	/**
+	 * Display a turn notice for a player
+	 * @param player
+	 */
+	public void turn (Player player) {
+		String print = player.getPrint();
+		
+		System.out.println('\n' + "========================================");
+		System.out.println("It's " + print + " their turn!");
+	}
 }
